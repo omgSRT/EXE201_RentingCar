fa3b565fe093471d885d2dbc7bd403f2a8bdad56
omgSRT <thangckdt@gmail.com>
2024-02-20 14:53:39 +0700

    feat: vehicle dao and repo

--- BusinessObjects/Models/VehicleImage.cs
+++ BusinessObjects/Models/VehicleImage.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BusinessObjects.Models
 {
     public partial class VehicleImage
     {
         public long ImagesId { get; set; }
+        [Required]
         public string? ImagesLink { get; set; }
         public long? VehicleId { get; set; }
 
+        [JsonIgnore]
         public virtual Vehicle? Vehicle { get; set; }
     }
 }
--- RentingCarRepositories/Repository/VehicleRepository.cs
+++ RentingCarRepositories/Repository/VehicleRepository.cs
@@ -1,4 +1,6 @@
-﻿using RentingCarRepositories.RepositoryInterface;
+﻿using BusinessObjects.Models;
+using RentingCarDAO;
+using RentingCarRepositories.RepositoryInterface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,59 @@ namespace RentingCarRepositories.Repository
 {
     public class VehicleRepository : IVehicleRepository
     {
+        private readonly VehicleDAO _vehicleDAO;
+        public VehicleRepository()
+        {
+            _vehicleDAO = new VehicleDAO();
+        }
+        public bool AddVehicle(Vehicle vehicle)
+        {
+            return _vehicleDAO.AddVehicle(vehicle);
+        }
+
+        public bool AddVehicleImage(VehicleImage image)
+        {
+            return _vehicleDAO.AddVehicleImage(image);
+        }
+
+        public Vehicle? GetVehicleById(long id)
+        {
+            return _vehicleDAO.GetVehicleById(id);
+        }
+
+        public VehicleImage? GetVehicleImageById(long id)
+        {
+            return _vehicleDAO.GetVehicleImageById(id);
+        }
+
+        public IEnumerable<VehicleImage> GetVehicleImages()
+        {
+            return _vehicleDAO.GetVehicleImages();
+        }
+
+        public IEnumerable<Vehicle> GetVehicles()
+        {
+            return _vehicleDAO.GetVehicles();
+        }
+
+        public bool RemoveVehicle(Vehicle vehicle)
+        {
+            return _vehicleDAO.RemoveVehicle(vehicle);
+        }
+
+        public bool RemoveVehicleImage(VehicleImage image)
+        {
+            return _vehicleDAO.RemoveVehicleImage(image);
+        }
+
+        public bool UpdateVehicle(Vehicle vehicle)
+        {
+            return _vehicleDAO.UpdateVehicle(vehicle);
+        }
+
+        public bool UpdateVehicleImage(VehicleImage image)
+        {
+            return _vehicleDAO.UpdateVehicleImage(image);
+        }
     }
 }
